b631b3c2f6d5f1b853a8ced8c7a6abf863895133
Hitezverma12 <hitezverma12@yahoo.com>
2025-04-03 18:43:02 +1100

    holiday.py

---
+++ vacation.py
@@ -0,0 +1,20 @@
+def hotel_cost(nights):
+    return 140*nights
+
+def plane_ride_cost(city):
+    if "Charlotte" == "city":
+        return 183
+    elif "Tampa" == "city":
+        return 220
+    elif "Pittsburgh" == "city":
+        return 222
+    elif "Los angeles" == "ctiy":
+            return 475
+    
+
+def rental_car_cost(days):
+     if days>=7 :
+          return 40*days - 50
+     elif days>=3 :
+          return
+
